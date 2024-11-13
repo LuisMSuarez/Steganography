6a33913302644bc53db6367df749dc701fe4cf8d
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-11-13 15:24:01 -0800

    logging

--- steganography.cpp
+++ steganography.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char **argv)
 {
-    std::cout << "hello world\r\n";
+    std::cout << "Loading bitmap\n";
     bmp::Bitmap mybitmap("sample.bmp");
+    std::cout << "Image size:" << mybitmap.height() << "X" << mybitmap.width() << "\n";
     return 0;
 }
