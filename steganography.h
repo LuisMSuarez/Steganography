0b0d176d0135b080440549dcc3c9f506d7f89cdd
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-11-15 13:20:45 -0800

    Command-line parsing (#4)
    * Doxygen comments and updated interfaces
    
    * command-line parsing

--- steganography.h
+++ steganography.h
@@ -13,13 +13,13 @@ namespace steganographyLib
             /// @param sourceDataFilePath Path to file that contains information that we wish to embed into bitmap.
             /// @param destinationBitmapDataFilePath Path to bitmap file that will be the result of embedding into the original bitmap file.
             /// @param bitsPerPixel Resolution that determines how many bits from each RGB pixel (24 bits) will encode source data.  Must be a multiple of 3 between 3 and 24. 
-            void embedInformation(const std::string &originalBitmapFilePath, const std::string &sourceDataFilePath, const std::string &destinationBitmapDataFilePath, u_int8_t bitsPerPixel);
+            void embed(const std::string &originalBitmapFilePath, const std::string &sourceDataFilePath, const std::string &destinationBitmapDataFilePath, u_int8_t bitsPerPixel);
             
             /// @brief Extracts information from a bitmap.
             /// @param sourceBitmapFilePath Path to file that contains information that we wish to extract from the bitmap.
             /// @param destinationDataFilePath Path to file that will be the result of extracting information from the original bitmap file.
             /// @param bitsPerPixel Resolution that determines how many bits from each RGB pixel (24 bits) encodes source data.  Must be a multiple of 3 between 3 and 24. 
-            void extractInformation(const std::string &sourceBitmapFilePath, const std::string &destinationDataFilePath, u_int8_t bitsPerPixel);
+            void extract(const std::string &sourceBitmapFilePath, const std::string &destinationDataFilePath, u_int8_t bitsPerPixel);
         private:
     };
 }
