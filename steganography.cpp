8e3482e12c49299aad94e1f32dd12e8be8e34d44
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-11-14 16:16:33 -0800

    Doxygen comments and updated interfaces

--- steganography.cpp
+++ steganography.cpp
@@ -9,3 +9,11 @@ steganographyLib::Steganography::Steganography()
 steganographyLib::Steganography::~Steganography()
 {
 }
+
+void steganographyLib::Steganography::embedInformation(const std::string &originalBitmapFilePath, const std::string &sourceDataFilePath, const std::string &destinationBitmapDataFilePath, u_int8_t bitsPerPixel)
+{
+}
+
+void steganographyLib::Steganography::extractInformation(const std::string &source, const std::string &destination, u_int8_t bitsPerPixel)
+{
+}
--- steganography.h
+++ steganography.h
@@ -7,8 +7,19 @@ namespace steganographyLib
         public:
             Steganography();
             ~Steganography();
-            void embedInformation();
-            void extractInformation();
+            
+            /// @brief Embeds information to a bitmap.
+            /// @param originalBitmapFilePath Path to original bitmap that will be used to embed information into its pixels. This bitmap will be opened in readonly mode.
+            /// @param sourceDataFilePath Path to file that contains information that we wish to embed into bitmap.
+            /// @param destinationBitmapDataFilePath Path to bitmap file that will be the result of embedding into the original bitmap file.
+            /// @param bitsPerPixel Resolution that determines how many bits from each RGB pixel (24 bits) will encode source data.  Must be a multiple of 3 between 3 and 24. 
+            void embedInformation(const std::string &originalBitmapFilePath, const std::string &sourceDataFilePath, const std::string &destinationBitmapDataFilePath, u_int8_t bitsPerPixel);
+            
+            /// @brief Extracts information from a bitmap.
+            /// @param sourceBitmapFilePath Path to file that contains information that we wish to extract from the bitmap.
+            /// @param destinationDataFilePath Path to file that will be the result of extracting information from the original bitmap file.
+            /// @param bitsPerPixel Resolution that determines how many bits from each RGB pixel (24 bits) encodes source data.  Must be a multiple of 3 between 3 and 24. 
+            void extractInformation(const std::string &sourceBitmapFilePath, const std::string &destinationDataFilePath, u_int8_t bitsPerPixel);
         private:
     };
 }
