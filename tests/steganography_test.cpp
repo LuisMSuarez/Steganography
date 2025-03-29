2563240743ef8a287bc03d3c72f774e681d69948
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-03-28 18:41:34 -0700

    Integrating CI build (#21)
    * callback tests
    
    * update to latest version of googletests
    
    * enabling qt support
    
    * yaml file for build
    
    * Update CPP-build.yml

--- tests/steganography_test.cpp
+++ tests/steganography_test.cpp
@@ -4,10 +4,10 @@
 
 using namespace steganographyLib;
 
-TEST(SteganographyTest, EmbedValidText) {
+TEST(SteganographyTest, EmbedValidText_NoCallback) {
     Steganography steg;
-    std::string sourceDataFilePath = "../../data/sampleInput.txt";
-    std::string originalBitmapFilePath = "../../data/sample.bmp";
+    std::string sourceDataFilePath = "../../../data/sampleInput.txt";
+    std::string originalBitmapFilePath = "../../../data/sample.bmp";
     std::string destinationDataFilePath = "embeddedBitmap_6bits.bmp";
     uint8_t bitsPerPixel = 6;
 
@@ -27,9 +27,44 @@ TEST(SteganographyTest, EmbedValidText) {
     std::filesystem::remove(destinationDataFilePath);
 }
 
-TEST(SteganographyTest, ExtractValidBitmap) {
+static bool embedValidText_Callback_invoked = false;
+void EmbedValidText_Callback_testCallback(int progressPercentage)
+{
+   embedValidText_Callback_invoked = true;
+}
+
+TEST(SteganographyTest, EmbedValidText_Callback) {
     Steganography steg;
-    std::string sourceBitmapFilePath = "../../data/embedded_6bits.bmp";
+    std::string sourceDataFilePath = "../../../data/sampleInput.txt";
+    std::string originalBitmapFilePath = "../../../data/sample.bmp";
+    std::string destinationDataFilePath = "embeddedBitmap_6bits.bmp";
+    uint8_t bitsPerPixel = 6;
+
+    // Ensure the source bitmap exists
+    ASSERT_TRUE(std::filesystem::exists(originalBitmapFilePath));
+
+    // Ensure the source data exists
+    ASSERT_TRUE(std::filesystem::exists(sourceDataFilePath));
+
+    // Register the callback
+    steg.registerProgressCallback(EmbedValidText_Callback_testCallback, 10);
+
+    // Call the extract method
+    EXPECT_NO_THROW(steg.embed(originalBitmapFilePath, sourceDataFilePath, destinationDataFilePath, bitsPerPixel));
+
+    // Verify the output file is created
+    EXPECT_TRUE(std::filesystem::exists(destinationDataFilePath));
+
+    // Verify that the callback was invoked
+    EXPECT_TRUE(embedValidText_Callback_invoked);
+
+    // Clean up
+    std::filesystem::remove(destinationDataFilePath);
+}
+
+TEST(SteganographyTest, ExtractValidBitmap_NoCallback) {
+    Steganography steg;
+    std::string sourceBitmapFilePath = "../../../data/embedded_6bits.bmp";
     std::string destinationDataFilePath = "output_data.txt";
     uint8_t bitsPerPixel = 6;
 
@@ -41,6 +76,37 @@ TEST(SteganographyTest, ExtractValidBitmap) {
 
     // Verify the output file is created
     EXPECT_TRUE(std::filesystem::exists(destinationDataFilePath));
+
+    // Clean up
+    std::filesystem::remove(destinationDataFilePath);
+}
+
+static bool extractValidBitmap_Callback_invoked = false;
+void ExtractValidBitmap_Callback_testCallback(int progressPercentage)
+{
+   extractValidBitmap_Callback_invoked = true;
+}
+
+TEST(SteganographyTest, ExtractValidBitmap_Callback) {
+    Steganography steg;
+    std::string sourceBitmapFilePath = "../../../data/embedded_6bits.bmp";
+    std::string destinationDataFilePath = "output_data.txt";
+    uint8_t bitsPerPixel = 6;
+
+    // Ensure the source bitmap exists
+    ASSERT_TRUE(std::filesystem::exists(sourceBitmapFilePath));
+
+    // Register the callback
+    steg.registerProgressCallback(ExtractValidBitmap_Callback_testCallback, 10);
+
+    // Call the extract method
+    EXPECT_NO_THROW(steg.extract(sourceBitmapFilePath, destinationDataFilePath, bitsPerPixel));
+
+    // Verify the output file is created
+    EXPECT_TRUE(std::filesystem::exists(destinationDataFilePath));
+
+    // Verify that the callback was invoked
+    EXPECT_TRUE(embedValidText_Callback_invoked);
 
     // Clean up
     std::filesystem::remove(destinationDataFilePath);
@@ -58,7 +124,7 @@ TEST(SteganographyTest, ExtractInvalidBitmapPath) {
 
 TEST(SteganographyTest, ExtractInvalidBitsPerPixel) {
     Steganography steg;
-    std::string sourceBitmapFilePath = "../../data/embedded_6bits.bmp";
+    std::string sourceBitmapFilePath = "../../../data/embedded_6bits.bmp";
     std::string destinationDataFilePath = "output_data.txt";
     uint8_t invalidBitsPerPixel = 5; // Not a multiple of 3
 
